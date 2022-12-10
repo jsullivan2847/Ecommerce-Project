080dad37e3207764f90e6863e8fef93534c54fb5
James Sullivan <jsullivan2847@gmail.com>
2022-12-10 18:34:15 -0500

    beginning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import './categories.styles.scss'
 
 function App() {
+
+  const categories = [
+   { 
+    title: "Hats",
+    id: 1,
+  },
+   { 
+    title: "Shirts",
+    id: 1,
+  },
+   { 
+    title: "Pants",
+    id: 1,
+  },
+   { 
+    title: "Jewelry",
+    id: 1,
+  },
+  ]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='categories-container'>
+        {categories.map(({title}) => {
+          return (
+             <div className='category-container'>
+             <div className='category-body-container'>
+             <h1>{title}</h1>
+             <p>Shop Now</p>
+           </div>
+          </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
